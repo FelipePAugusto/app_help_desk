8a7e8f6b6b903254d2cda3c50c40f897992f2765
unknown <felipe.bsi.up@gmail.com>
2022-04-30 21:36:33 +0100

    Criacao das rotas SignIn e Register

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { SignIn } from '../screens/SignIn';
+// import { SignIn } from '../screens/SignIn';
+// import { Register } from '@screens/Register';
+import { Register } from '../screens/Register';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AuthRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="signIn" component={SignIn} />
+      <Screen name="register" component={Register} />
+    </Navigator>
+  );
+}
