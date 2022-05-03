c829b550f478a8abccdc0f7d995158899d59b9cf
unknown <felipe.bsi.up@gmail.com>
2022-05-03 21:13:20 +0100

    Ajustes na biblioteca Reanimated / Screens / babel.config / package.json

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
@@ -21,7 +20,8 @@ module.exports = function(api) {
             '@hooks': './src/hooks',
           }
         }
-      ]
+      ],
+      'react-native-reanimated/plugin'
     ]
   };
 };
