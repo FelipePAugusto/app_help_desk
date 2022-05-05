22706e684c5455cec24177628dd78af69f048b24
unknown <felipe.bsi.up@gmail.com>
2022-05-05 21:35:42 +0100

    Resolucao do problema de bibliotecas descontinuadas / react-native-reanimated / @gorhom/bottom-sheet

--- src/components/Controllers/NewOrder/index.tsx
+++ src/components/Controllers/NewOrder/index.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import React, { useRef } from 'react';
 import { BottomSheetView, BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
